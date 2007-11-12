445bf01ff400a82fa09a551079582ffc8dc96aa6
andy <andy@85f9bd53-ed1e-0410-abaf-ef5a3fdb13a8>
2007-11-12 05:38:07 +0000

    Kill some dead code. Rearrange Render.handlePixel for clarity and uniformity with the rest of the source.
    git-svn-id: http://www.verge-rpg.com/svn/verge3/trunk@176 85f9bd53-ed1e-0410-abaf-ef5a3fdb13a8

--- tools/maped3/app/pr2/Render.cs
+++ tools/maped3/app/pr2/Render.cs
@@ -4,165 +4,6 @@ using System.Drawing.Imaging;
 
 namespace winmaped2 {
     public sealed class Render {
-#if false
-        public unsafe class pr2.RenderImage : IDisposable {
-            enum Variety {
-                _Buffer, _Bitmap
-            };
-            
-            Variety variety;
-
-            BitmapData bitmapData;
-            Bitmap bmp;
-            bool bDisposed;
-            int* buf;
-            int width;
-            int height;
-            int pitch;
-            int stride;
-
-            public pr2.RenderImage() {
-                bDisposed = false;
-            }
-
-            public int* Pixels {
-                get {
-                    return buf;
-                }
-            }
-
-            public int Pitch {
-                get {
-                    return pitch;
-                }
-            }
-
-            public int Stride {
-                get {
-                    return stride;
-                }
-            }
-
-            public int Width {
-                get {
-                    return width;
-                }
-            }
-
-            public int Height {
-                get {
-                    return height;
-                }
-            }
-
-            public unsafe void Dispose() {
-                if (!bDisposed) {
-                    bDisposed = true;
-                    if (variety == Variety._Bitmap) {
-                        bmp.UnlockBits(bitmapData);
-                    } else if (variety == Variety._Buffer) {
-                        System.Runtime.InteropServices.Marshal.FreeHGlobal((IntPtr)buf);
-                    }
-                }
-            }
-
-            public unsafe int[] getArray() {
-                int[] ret = new int[width * height];
-
-                for (int y = 0; y < height; y++) {
-                    for (int x = 0; x < width; x++) {
-                        ret[y * width + x] = buf[y * pitch + x];
-                    }
-                }
-
-                return ret;
-            }
-
-            public unsafe Bitmap getBitmap() {
-                int w = width;
-                int h = height;
-                int p = pitch;
-                Bitmap bmp = new Bitmap(w, h, PixelFormat.Format32bppArgb);
-                BitmapData bmpd = bmp.LockBits(new Rectangle(0, 0, w, h), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
-                int* data = (int*)bmpd.Scan0.ToPointer();
-
-                int j = 0;
-                for (int y = 0; y < h; y++) {
-                    for (int x = 0; x < w; x++) {
-                        data[j++] = buf[y * p + x];
-                    }
-                }
-                bmp.UnlockBits(bmpd);
-                return bmp;
-            }
-
-            public unsafe void clear(int color) {
-                int* data = buf;
-                int xadd = pitch - width;
-                for (int y = 0; y < height; y++) {
-                    for (int x = 0; x < width; x++)
-                        *data++ = color;
-                    data += xadd;
-                }
-            }
-
-            public unsafe int getPixel(int x, int y) {
-                return buf[y * pitch + x];
-            }
-
-            public unsafe static pr2.RenderImage create(Bitmap bmp) {
-                int w = bmp.Width;
-                int h = bmp.Height;
-                pr2.RenderImage img = create(w, h);
-
-                BitmapData bmpd = bmp.LockBits(new Rectangle(0, 0, w, h), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
-
-                int dp = img.pitch;
-                int sp = bmpd.Stride / 4;
-
-                int* srcdata = (int*)bmpd.Scan0.ToPointer();
-                int* destdata = (int*)img.Pixels;
-
-                for (int y = 0; y < h; y++) {
-                    for (int x = 0; x < w; x++) {
-                        destdata[y * dp + x] = srcdata[y * sp + x];
-                    }
-                }
-                bmp.UnlockBits(bmpd);
-
-                return img;
-            }
-
-            public unsafe static pr2.RenderImage create(int width, int height) {
-                pr2.RenderImage img = new pr2.RenderImage();
-                img.variety = Variety._Buffer;
-                img.width = width;
-                img.height = height;
-                img.buf = (int*)System.Runtime.InteropServices.Marshal.AllocHGlobal(width * height * 4);
-                img.stride = width * 4;
-                img.pitch = width;
-                return img;
-            }
-
-            public unsafe static pr2.RenderImage lockBitmap(Bitmap bmp) {
-                ImageLockMode imageLockMode = ImageLockMode.ReadWrite;
-                pr2.RenderImage img = new pr2.RenderImage();
-                img.variety = Variety._Bitmap;
-                img.bmp = bmp;
-                img.bitmapData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), imageLockMode, PixelFormat.Format32bppArgb);
-
-                img.buf = (int*)img.bitmapData.Scan0.ToPointer();
-                img.width = img.bitmapData.Width;
-                img.height = img.bitmapData.Height;
-                img.stride = img.bitmapData.Stride;
-                img.pitch = img.stride / 4;
-
-                return img;
-            }
-
-        }
-#endif
-
         private Render() { }
 
         public static Bitmap createBitmap(int width, int height) {
@@ -237,19 +78,14 @@ namespace winmaped2 {
             return false;
         }
 
-        private static void handlePixel(int src, ref int dest, int op, bool mixFlag, bool tflag) {
+        private static void handlePixel(int src, ref int dest, int op, bool mixFlag, bool drawZero) {
             if (mixFlag) {
-                if (tflag) {
-                    if ((src & 0x00FFFFFF) != 0x00FF00FF) {
-                        dest = mixPixel(src, dest, op);
-                    }
-                } else dest = mixPixel(src, dest, op);
+                src = mixPixel(src, dest, op);
+            }
+            if (drawZero) {
+                dest = src;
             } else {
-                if (tflag) {
-                    if ((src & 0x00FFFFFF) != 0x00FF00FF) {
-                        dest = src;
-                    }
-                } else {
+                if ((src & 0x00FFFFFF) != 0x00FF00FF) {
                     dest = src;
                 }
             }
@@ -303,7 +139,7 @@ namespace winmaped2 {
 
             for (; ylen != 0; ylen--) {
                 for (int xx = 0; xx < xlen; xx++) {
-                    handlePixel(s[xx], ref d[xx], 0, false, !drawZero);
+                    handlePixel(s[xx], ref d[xx], 0, false, drawZero);
                 }
 
                 s += spitch;
@@ -381,7 +217,7 @@ namespace winmaped2 {
 
             for (; ylen > 0; ylen--) {
                 for (int x = 0; x < xlen; x++) {
-                    handlePixel(color, ref d[x], (int)Render.PixelOp.Alpha50, true, false);
+                    handlePixel(color, ref d[x], (int)Render.PixelOp.Alpha50, true, true);
                 }
 
                 d += dpitch;
@@ -467,7 +303,7 @@ namespace winmaped2 {
                 for (; ylen > 0; ylen--) {
                     for (int x = 0; x < xlen; x++) {
                         if (s[x] != 0) {
-                            handlePixel(color, ref d[x], (int)Render.PixelOp.Alpha50, true, false);
+                            handlePixel(color, ref d[x], (int)Render.PixelOp.Alpha50, true, true);
                         }
                     }
 
@@ -533,7 +369,7 @@ namespace winmaped2 {
                 if (y + y0 >= 0 && y + y0 < bh) {
                     for (int x = 0; x < w; x++) {
                         if (x + x0 >= 0 && x + x0 < bw) {
-                            handlePixel(color, ref img.Pixels[(y0 + y) * bp + x0 + x], (int)op, true, false);
+                            handlePixel(color, ref img.Pixels[(y0 + y) * bp + x0 + x], (int)op, true, true);
                         }
                     }
                 }
